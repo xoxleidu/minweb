9a701c5daac91d4e4095ffe0ebb0cb39b8065375
xoxleidu <sfgduleiddd@163.com>
2018-09-17 23:26:12 +0800

    20180217

--- src/main/java/com/example/minweb/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.minweb.repository;
-
-import com.example.minweb.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book,Integer> {
-
-}
